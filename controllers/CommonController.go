1283ba45a43c8a594c399dce4e55a54ec5c2b157
Valeri Hristov <vhristov@vmware.com>
2020-09-03 17:00:43 +0300

    post resource functionality

--- controllers/CommonController.go
+++ controllers/CommonController.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"goendpoint/services"
 	"net/http"
+	"io/ioutil"
 )
 
 func AttachHandlers(resource string) {
@@ -18,6 +19,35 @@ func AttachHandlers(resource string) {
 		resp.Write(allItems)
 	}
 
-	http.HandleFunc("/" + resource, getAllHandler)
+	addResource := func(resp http.ResponseWriter, req *http.Request) {
+		// TODO: Replace with MaxBytesReader
+		body, err := ioutil.ReadAll(req.Body)
+
+		if err != nil {
+			http.Error(resp, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		r, saveToDiskErr := services.Add(resource, body)
+
+		if saveToDiskErr != nil {
+			http.Error(resp, saveToDiskErr.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		resp.Header().Set("Content-Type", "application/json")
+		resp.Write(r)
+	}
+
+	dispatcher := func(resp http.ResponseWriter, req *http.Request) {
+		switch req.Method {
+		case http.MethodGet:
+			getAllHandler(resp, req)
+		case http.MethodPost:
+			addResource(resp, req)
+		}
+	}
+
+	http.HandleFunc("/" + resource, dispatcher)
 }
 
--- services/PersistanceService.go
+++ services/PersistanceService.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"encoding/json"
+	"goendpoint/models"
 	"io/ioutil"
 	"path/filepath"
 	"goendpoint/utils"
@@ -25,7 +26,58 @@ func GetAll(resource string) ([]byte, error) {
 	return jsonStr, nil
 }
 
+func Add(resource string, data []byte) (r []byte, e error) {
+	var incomingData interface{}
+	err := json.Unmarshal(data, &incomingData)
+
+	if err != nil {
+		e = err
+		return
+	}
+
+	data, loadErr := loadResourceFile(resource)
+
+	if loadErr != nil {
+		e = loadErr
+		return
+	}
+
+	var resourceSchema models.Schema
+
+	resourceSchemaLoadErr := json.Unmarshal(data, &resourceSchema)
+
+	if resourceSchemaLoadErr != nil {
+		e = resourceSchemaLoadErr
+		return
+	}
+
+	resourceSchema.Data = append(resourceSchema.Data, incomingData)
+
+	jsStr, marshallingErr := json.Marshal(resourceSchema)
+
+	if marshallingErr != nil {
+		e = marshallingErr
+		return
+	}
+
+	writeToFileErr := persist(resource, jsStr)
+
+	if writeToFileErr != nil {
+		e = writeToFileErr
+		return
+	}
+
+	incomingDataStr, _ := json.Marshal(incomingData)
+
+	r = incomingDataStr
+	return
+}
+
 func loadResourceFile(resource string) (s []byte, e error) {
 	s, e = ioutil.ReadFile(filepath.Join("db", resource + ".json"))
 	return
+}
+
+func persist(resource string, data []byte) (e error) {
+	e = ioutil.WriteFile(filepath.Join("db", resource + ".json"), data, 0755)
 }
